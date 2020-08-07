06ba6dc55c34ce08312fc13c50c61cf040c83f50
Cassidy Williams <cassidoo@users.noreply.github.com>
2020-08-06 21:14:46 -0700

    Livestream demo of hooks

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
-function App() {
+// class Something extends React.Component {
+//   constructor() {
+//     super()
+//     this.state = {
+//       count: 0
+//     }
+//   }
+
+//   componentDidMount() {
+//     // call an API
+//   }
+
+//   componentShouldUpdate(prevProps) {
+//     // should the component update?
+//   }
+
+//   componentWillUnmount() {
+//     // going away!
+//   }
+
+//   increment() {
+//     this.setState({ count: count + 1 })
+//   }
+
+//   render() {
+//     return <div>{this.state.count}</div>
+//   }
+// }
+
+function useDocumentTitle(count) {
+  useEffect(() => {
+    document.title = "The count is: " + count;
+
+    return () => {
+      // unmounting!
+    };
+  }, [count]); // dependency array
+
+  // [] = On mount
+  // [variable] = On mount, whenever
+  //              variable is updated
+  // no array = On mount, whenever state changes
+}
+
+function App(props) {
+  let [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  useDocumentTitle(count);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={increment}>Add</button>
+      {count}
     </div>
   );
 }
